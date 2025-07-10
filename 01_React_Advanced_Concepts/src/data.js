d2aaa4b3003b5cbbb71a370c267a4b3be7ef7e2b
harrynyrrah <harrynyrrah@gmail.com>
2025-07-10 20:03:05 +0545

    projet setup

---
+++ 01_React_Advanced_Concepts/src/data.js
@@ -0,0 +1,23 @@
+export const data = [
+  { id: 1, name: "karan" },
+  { id: 2, name: "bhupi" },
+  { id: 3, name: "sanjok" },
+  { id: 4, name: "kappu" },
+];
+
+export const people = [
+  { id: 1, name: "Bhupendra", nickName: "Bhupi" },
+  { id: 2, name: "Vivek" },
+  {
+    id: 3,
+    name: "Manoj",
+    images: [
+      {
+        small: {
+          url: "https://res.cloudinary.com/diqqf3eq2/image/upload/ar_1:1,bo_5px_solid_rgb:ff0000,c_fill,g_auto,r_max,w_1000/v1595959121/person-1_aufeoq.jpg",
+        },
+      },
+    ],
+  },
+  { id: 4, name: "Shyam" },
+];
